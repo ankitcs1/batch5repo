9ce1b4832ba212d04bf7cc3ca777e80424f9acf7
localadmin <localadmin@DESKTOP-3SBJS7L>
2025-02-19 11:12:04 +0530

    second commit

---
+++ gatewaybatch5/src/test/java/com/example/gatewaybatch5/Gatewaybatch5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewaybatch5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gatewaybatch5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
